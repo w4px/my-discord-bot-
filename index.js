22435e83ede790fcea1544ca8394ce52c6ea650a
w4px <fiveestoree2002@gmail.com>
2026-07-11 15:30:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 
+// إعدادات الصلاحيات للبوت ليتمكن من قراءة الرسائل
 const client = new Client({ 
     intents: [
         GatewayIntentBits.Guilds, 
@@ -9,14 +10,20 @@ const client = new Client({
     ] 
 });
 
+// رسالة عند تشغيل البوت
 client.once('ready', () => {
-    console.log(`البوت يعمل الآن باسم ${client.user.tag}!`);
+    console.log(`تم تسجيل الدخول بنجاح باسم ${client.user.tag}!`);
 });
 
+// الرد على الرسائل
 client.on('messageCreate', (message) => {
+    // تجاهل رسائل البوت نفسه
+    if (message.author.bot) return;
+
     if (message.content === '!ping') {
         message.reply('البوت يعمل بكفاءة!');
     }
 });
 
+// تسجيل الدخول باستخدام التوكن من إعدادات Render
 client.login(process.env.TOKEN);
